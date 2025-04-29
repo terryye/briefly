1e1383f227afb8f8b3dce02152f0cd6200cfd057
terryye <i@terryye.com>
2025-04-29 16:57:10 -0700

    Email Auth

--- server/auth/config.ts
+++ server/auth/config.ts
@@ -2,6 +2,8 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import EmailProvider from "next-auth/providers/nodemailer";
+
 import { db } from "@/server/db";
 import {
     accounts,
@@ -19,7 +21,22 @@ declare module "next-auth" {
 }
 
 export const authConfig = {
-    providers: [GitHub, Google],
+    providers: [
+        EmailProvider({
+            server: {
+                host: process.env.EMAIL_SERVER_HOST,
+                port: process.env.EMAIL_SERVER_PORT,
+                auth: {
+                    user: process.env.EMAIL_SERVER_USER,
+                    pass: process.env.EMAIL_SERVER_PASSWORD,
+                },
+            },
+            from: process.env.EMAIL_FROM,
+            name: "Email",
+        }),
+        Google,
+        GitHub,
+    ],
     adapter: DrizzleAdapter(db, {
         usersTable: users,
         accountsTable: accounts,
