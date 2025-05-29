57443f175cc29518112f041f957eecaea148d50f
terryye <i@terryye.com>
2025-05-28 18:21:24 -0700

    add tooltips

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -1,5 +1,5 @@
-import Icons from "./Icons";
 import { api } from "@/trpc/server";
+import Icons from "./Icons";
 
 const Badge = async () => {
     const badge = await api.summary.badge();
@@ -7,14 +7,22 @@ const Badge = async () => {
     return (
         <>
             <div className="basis-1/3 flex flex-row justify-end">
-                <div
-                    className="badge badge-success mr-2 text-white"
-                    title="Last 31 days"
-                >
-                    {Icons.hot} {badge?.last31days}
+                <div className="tooltip tooltip-bottom" data-tip="Last 31 days">
+                    <div
+                        className="badge badge-success mr-2 text-white"
+                        title="Last 31 days"
+                    >
+                        {Icons.hot} {badge?.last31days}
+                    </div>
                 </div>
-                <div className="badge badge-info  text-white" title="All time">
-                    {Icons.star} {badge?.allTime}
+
+                <div className="tooltip tooltip-bottom" data-tip="All time">
+                    <div
+                        className="badge badge-info  text-white"
+                        title="All time"
+                    >
+                        {Icons.star} {badge?.allTime}
+                    </div>
                 </div>
             </div>
         </>
